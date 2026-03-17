b6d72751e3ad3c6a983023c304ad0243e0b00db3
NubPlayz <182065584+NubPlayz@users.noreply.github.com>
2026-03-17 20:36:29 +0530

    addded retries so it doesnt quite when failed the first time

--- script/index1.js
+++ script/index1.js
@@ -1,41 +1,54 @@
 import fs from "fs";
 
-const batchSize=10;
-const totalPages=2217;
-const allItems=[];
+const batchSize = 10;
+const totalPages = 2217;
+const allItems = [];
 console.time(`time taken:`);
 
-for(let i=1; i<=totalPages; i+=batchSize){
-  
-    
+for (let i = 1; i <= totalPages; i += batchSize) {
 
-    const batch=[];
-    for(let page=i; page<i+batchSize && page <=totalPages;page++){
-        
-        
+
+
+    const batch = [];
+    for (let page = i; page < i + batchSize && page <= totalPages; page++) {
+
+
 
         batch.push(
-            (async ()=> {
-                const res= await fetch(`https://backend.delhimetrorail.com/api/v2/en/lost_found_items/?page=${page}`);
+            (async () => {
+                let attemps = 0;
+                let res;
+                while (attemps < 3) {
+                    try {
+                        res = await fetch(`https://backend.delhimetrorail.com/api/v2/en/lost_found_items/?page=${page}`);
+                        if (res.ok) break;
 
-                if(!res.ok) {
-                    console.error(res)
-                    throw new Error(`Failed page ${page}`);
+                        console.error(res)
+                        throw new Error(`Failed page ${page}`);
+                    }
+                    catch (error) {
+                        console.error(error)
+                        attemps++;
+                        continue;
+                    }
 
-            
+
 
                 }
+                if (!res?.ok) {
+                    throw new Error(`Failed page ${page}`);
+                }
 
-                return  res.json();
+                return res.json();
             })()
         )
 
     }
 
-    const results= await Promise.all(batch);
+    const results = await Promise.all(batch);
 
-    for(const data of results){
-    allItems.push(...data.results);
+    for (const data of results) {
+        allItems.push(...data.results);
 
     }
 }
@@ -45,7 +58,7 @@ console.log(allItems.length);
 
 fs.writeFileSync(
     new URL("./allItems.json", import.meta.url),
-    JSON.stringify(allItems, null , 2 ),
+    JSON.stringify(allItems, null, 2),
     `utf-8`
 );
 
