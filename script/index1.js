f1c8206940d63567bf46200c7a7478e47c5b22a5
NubPlayz <182065584+NubPlayz@users.noreply.github.com>
2026-03-17 20:55:05 +0530

    fixed the cloudfare block

--- script/index1.js
+++ script/index1.js
@@ -26,7 +26,8 @@ for (let i = 1; i <= totalPages; i += batchSize) {
                                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
                                     "Accept": "application/json, text/plain, */*",
                                     "Accept-Language": "en-US,en;q=0.9",
-
+                                    "Referer": "https://www.delhimetrorail.com/",
+                                    "Origin": "https://www.delhimetrorail.com"
                                 }
                             }
                         );
